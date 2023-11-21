88dc23c266161e9e5b3a917deb1e7d6cf14c41f8
YuriHenriqueRezende <yuri.henrique.rezende@outlook.com.br>
2023-11-21 20:51:30 -0300

    commit

---
+++ filmes/layouts/layout-login.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <img src="https://yt3.googleusercontent.com/hdXNOnWTo5lN1of-tJyiQPVZVda5OTGf9IXlcyLCYUAnMpmYHIlQlo8moBu_52EcRIEj9XUuH4=s176-c-k-c0x00ffffff-no-rj">
+        <slot></slot>
+    </div>
+</template>
---
+++ filmes/pages/assinaturas/index.vue
@@ -0,0 +1,19 @@
+<script setup>
+    const { data: subscriptions } = await useFetch('http://localhost:8000/assinatura');
+
+    console.log(subscriptions.value)
+</script>
+
+
+
+
+<template>
+    <div>
+        <h1>Assinaturas Disponiveis</h1>
+
+        <section v-for="assinatura in subscriptions" 
+        :key="assinatura.id">
+            <p>{{ assinatura.id }}</p>
+        </section>   
+    </div>
+</template>
