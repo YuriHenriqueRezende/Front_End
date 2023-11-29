996d5a7af5a67b15e4ed23fe38fdb54f670d3007
YuriHenriqueRezende <yuri.henrique.rezende@outlook.com.br>
2023-11-28 23:56:07 -0300

    Update index.vue

--- somativa/pages/index.vue
+++ somativa/pages/index.vue
@@ -1,29 +1,34 @@
 <script setup>
+  import { ref } from 'vue';
 
-    const { data: login } = await useFetch('http://localhost:8000/usuarios');
-    let fazerlogin;
+  const { data: login } = await useFetch('http://localhost:8000/usuarios');
+  const selectedUser = ref(null);
 
-    const save = async () => {
-        
+  const save = async () => {
+    if (selectedUser.value) {
+      // A lógica para lidar com o botão sendo clicado quando uma opção está selecionada
+      console.log('Usuário selecionado:', selectedUser.value);
+    } else {
+      // A lógica para lidar com o botão sendo clicado sem nenhuma opção selecionada
+      console.warn('Selecione um usuário antes de entrar.');
     }
-
+  };
 </script>
 
 <template>
-        <h1>FAÇA LOGIN</h1>
-    <div>        
-
   <div>
-    <label for="">Selecione o usuário:</label>
-    <select v-model="fazerlogin">
-            <option v-for="logon in login.data" :key="logon.id" :value="logon.email">
-                {{ logon.email }} 
-            </option>                    
-    </select>
-    <br><br>
-    
-    <button @click="save">ENTRAR</button>
-
-  </div>
+    <h1>FAÇA LOGIN</h1>
+    <div>
+      <div>
+        <label for="">Selecione o usuário:</label>
+        <select v-model="selectedUser">
+          <option v-for="logon in login.data" :key="logon.id" :value="logon.email">
+            {{ logon.email }}
+          </option>
+        </select>
+        <br><br>
+        <button @click="save" v-if="selectedUser">ENTRAR</button>
+      </div>
     </div>
+  </div>
 </template>
