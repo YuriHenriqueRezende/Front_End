406ee9b0ef66a101d16d04f86be56327d9d31206
YuriHenriqueRezende <yuri.henrique.rezende@outlook.com.br>
2023-11-28 22:29:37 -0300

    commit

---
+++ somativa/pages/index.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+    const { data: login } = await useFetch('http://localhost:8000/usuarios');
+    let fazerlogin;
+
+    const save = async () => {
+        
+    }
+
+</script>
+
+<template>
+        <h1>FAÇA LOGIN</h1>
+    <div>        
+
+  <div>
+    <label for="">Selecione o usuário:</label>
+    <select v-model="fazerlogin">
+            <option v-for="logon in login.data" :key="logon.id" :value="logon.email">
+                {{ logon.email }} 
+            </option>                    
+    </select>
+    <br><br>
+    
+    <button @click="save">ENTRAR</button>
+
+  </div>
+    </div>
+</template>
