ed72b8883684b7bda2647763af9a13cf1d7f12fd
YuriHenriqueRezende <yuri.henrique.rezende@outlook.com.br>
2023-11-29 20:47:03 -0300

    commit

--- somativa/pages/tarefas/index.vue
+++ somativa/pages/tarefas/index.vue
@@ -6,11 +6,12 @@
 
 
 
-
+aaa
 <template>
     <div>
         <NuxtLink to="/">login</NuxtLink> <br>
         <NuxtLink to="/tarefas">tarefas</NuxtLink> <br>
+        <NuxtLink to="/detalhado">detalhado</NuxtLink> <br>
         <NuxtLink to="/ambientes">ambientes</NuxtLink> <br>
         <NuxtLink to="/cargos">cargos</NuxtLink> <br>
     </div>
