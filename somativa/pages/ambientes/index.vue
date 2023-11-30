c699173d328035066f67222d235899727b633a4a
YuriHenriqueRezende <yuri.henrique.rezende@outlook.com.br>
2023-11-30 01:30:17 -0300

    Update index.vue

--- somativa/pages/ambientes/index.vue
+++ somativa/pages/ambientes/index.vue
@@ -12,11 +12,15 @@
         <NuxtLink to="/tarefas">tarefas</NuxtLink> <br>
         <NuxtLink to="/ambientes">ambientes</NuxtLink> <br>
         <NuxtLink to="/cargos">cargos</NuxtLink> <br>
+        <h1>Cadastro de Ambientes:</h1>
+
+
+        
 
         <h1>Ambientes:</h1>
         <section v-for="ambiente in ambientes.data" 
         :key="ambiente.nome">
-            <p>{{ ambiente.nome }}</p>
+            <p>-{{ ambiente.nome }}</p>
         </section> 
 
     </div>
