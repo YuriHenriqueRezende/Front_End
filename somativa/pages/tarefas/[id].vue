92bd71b6f37b82aac0d84ff334d5302a7b0a3a03
YuriHenriqueRezende <yuri.henrique.rezende@outlook.com.br>
2023-11-29 23:54:22 -0300

    Update [id].vue

--- somativa/pages/tarefas/[id].vue
+++ somativa/pages/tarefas/[id].vue
@@ -1,5 +1,21 @@
+<script setup>
+    const route = useRoute();
 
+const {data: tarefa} = 
+await useFetch(`http://localhost:8000/tarefas/${route.params.id}`,{
+    key: 'tarefa'
+});
+  console.log(tarefa);
+</script>
 
 <template>
-    <h1>TESTE</h1>
+    <div>
+        <NuxtLink to="/">login</NuxtLink> <br>
+        <NuxtLink to="/tarefas">tarefas</NuxtLink> <br>
+        <NuxtLink to="/detalhado">detalhado</NuxtLink> <br>
+        <NuxtLink to="/cargos">cargos</NuxtLink> <br>
+    </div>
+    
+    <h1>Detalhes:</h1>
+            <p>{{ tarefa.data.nome }}</p>
 </template>
