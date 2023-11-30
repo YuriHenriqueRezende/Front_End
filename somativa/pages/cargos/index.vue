4584a47ef56a3aa816449801215190ed25e07d88
YuriHenriqueRezende <yuri.henrique.rezende@outlook.com.br>
2023-11-29 23:31:25 -0300

    commit

--- somativa/pages/cargos/index.vue
+++ somativa/pages/cargos/index.vue
@@ -8,7 +8,6 @@
     <div>
         <NuxtLink to="/">login</NuxtLink> <br>
         <NuxtLink to="/tarefas">tarefas</NuxtLink> <br>
-        <NuxtLink to="/detalhado">detalhado</NuxtLink> <br>
         <NuxtLink to="/ambientes">ambientes</NuxtLink> <br>
         <NuxtLink to="/cargos">cargos</NuxtLink> <br>
     </div>
--- somativa/pages/tarefas/index.vue
+++ somativa/pages/tarefas/index.vue
@@ -11,7 +11,6 @@
         <NuxtLink to="/">login</NuxtLink> <br>
         <NuxtLink to="/tarefas">tarefas</NuxtLink> <br>
         <NuxtLink to="/detalhado">detalhado</NuxtLink> <br>
-        <NuxtLink to="/ambientes">ambientes</NuxtLink> <br>
         <NuxtLink to="/cargos">cargos</NuxtLink> <br>
     </div>
     <h1>Tarefas</h1>
