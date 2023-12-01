16721cb304d1030d63f4ef3d7756df9c97f44856
YuriHenriqueRezende <yuri.henrique.rezende@outlook.com.br>
2023-11-30 21:34:18 -0300

    Update index.vue

--- somativa/pages/ambientes/index.vue
+++ somativa/pages/ambientes/index.vue
@@ -2,13 +2,28 @@
   const { data: ambientes } = await useFetch('http://localhost:8000/ambientes');
   console.log(ambientes)
 
-</script>
+  let show = false;
+    const setbutton = () => {
+        //alert("Cliquei no botão!")
+        show = true;
+        refreshNuxtData()
+    } 
 
+</script>
 
 <template>
     <div>
         <h1>Cadastro de Ambientes:</h1>
+        <p>Nome: </p>
+        <input type="text">
+        <button @click="setbutton">Adicionar</button>
+        
+        <section v-if="show === true">
+            
 
+        </section>
+
+        
         <h1>Ambientes:</h1>
         <section v-for="ambiente in ambientes.data" 
         :key="ambiente.nome">
