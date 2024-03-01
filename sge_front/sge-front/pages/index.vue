9b28aea8d0a1d2fd562d168d0616254ff83f2c97
YuriHenriqueRezende <yuri.henrique.rezende@outlook.com.br>
2024-02-29 22:00:10 -0300

    commit

---
+++ sge_front/sge-front/pages/index.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <main class="login_main">
+        <section class="logo_panel">
+
+
+        </section>
+        <section class="login_panel">
+            <h1>Sou Louco</h1>
+        </section>
+    </main>
+</template>
+
+<style lang="scss">
+    .login_main{
+        width: 100vw;
+        height: 100vw;
+        background-color: red;
+    }
+</style>
