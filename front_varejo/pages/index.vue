46f4131de49d9d7daf6c20e8a4de057618f24f2b
YuriHenriqueRezende <yuri.henrique.rezende@outlook.com.br>
2024-03-07 21:53:49 -0300

    commit

---
+++ front_varejo/pages/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+    import { ref, computed, reactive } from 'vue'
+
+
+</script>
+
+<template>
+    <main class="title">
+        <section></section>
+        <h1>Varejão do TIÃO DO GÁS</h1>
+    </main>
+
+
+
+</template>
+
+<style scoped lang="scss">
+
+    
+
+</style>
--- sge_front/sge-front/nuxt.config.ts
+++ sge_front/sge-front/nuxt.config.ts
@@ -1,9 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  typescript: {
+    typeCheck: true
+  },
+  modules: [
+    'nuxt-primevue',
+    
+  ],
+  primevue: {
+    components: {
+      include: ['Button']
+    }
+  },
   css: [
-    '~/assets/global.scss',
-    '~/assets/global-variables.scss'
-
+    'primeicons/primeicons.css', //css dos ícones do primevue
+    'primevue/resources/themes/aura-light-green/theme.css', // css tema dos componentes primevue
+    '~/assets/style/global-variables.scss',  //variáveis css global customizado para toda a aplicação
+    '~/assets/style/global-project.scss',  //css global customizado para toda a aplicação
   ]
 })
