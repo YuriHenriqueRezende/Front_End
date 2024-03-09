39b6e95fa564dce1efdfa0941a635ec9c38a2e70
YuriHenriqueRezende <yuri.henrique.rezende@outlook.com.br>
2024-03-09 16:46:18 -0300

    commit

--- front_varejo/pages/index.vue
+++ front_varejo/pages/index.vue
@@ -3,10 +3,33 @@
 </script>
 
 <template>
-    <main></main>
-    <h1>VAREJÃO DO TIÃO GAS</h1>
+    <main class="bar_up login_main">
+        <div>
+            <h1 class="color">VAREJÃO DO TIÃO GÁS</h1>
+        </div>
+        <select class="position">REDES SOCIAIS</select>
+
+    </main>
+
 </template>
 
 <style scoped lang="sass">
+    .bar_up
+        display: flex
+        flex-direction: row
+        align-items: center
+        justify-content: left
+
+    .login_main
+        width: 100vw
+        height: 10vh       
+        background-color: var(--dark-background-color)
+    
+    .color
+        color: #dedede
+    
+    .position
+        align-items: center
+        justify-content: center
 
 </style>
