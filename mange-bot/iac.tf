e79a113adbe700b2d94f5d69d72591238ddad2cb
YuriHenriqueRezende <yuri.henrique.rezende@outlook.com.br>
2024-10-03 18:53:14 -0300

    commit

--- mange-bot/iac.tf
+++ mange-bot/iac.tf
@@ -9,30 +9,38 @@ terraform {
 
 provider "azurerm" {
   resource_provider_registrations = "none"
-  subscription_id = "481c1d25-e4df-4209-9f20-6d3c0c28a214"
+  subscription_id = "250ae9c3-6c33-4030-b72a-ed22fce22920"
   features {    
   }
 }
 
-resource "azurerm_resource_group" "andre-savedra-robot-t2-group" {
-  name = "andre-savedra-robot-t2-group"
+resource "azurerm_resource_group" "yuri-bot-t-group" {
+  name = "yuri-bot-t-group"
   location = "eastus2"
 }
 
-resource "azurerm_service_plan" "andre-savedra-robot-t2-sp" {
-    name = "andre-savedra-robot-t2-sp"
-    resource_group_name = azurerm_resource_group.andre-savedra-robot-t2-group.name
-    location = azurerm_resource_group.andre-savedra-robot-t2-group.location
-    sku_name = "F1"
+resource "azurerm_service_plan" "yuri-bot-t-sp" {
+    name = "yuri-bot-t-sp"
+    resource_group_name = azurerm_resource_group.yuri-bot-t-group.name
+    location = azurerm_resource_group.yuri-bot-t-group.location
+    sku_name = "S1"
     os_type = "Windows"
 }
 
-resource "azurerm_windows_web_app" "andre-savedra-robot-t2-app" {
-  name = "andre-savedra-robot-t2-app"
-  resource_group_name = azurerm_resource_group.andre-savedra-robot-t2-group.name
-  location = azurerm_resource_group.andre-savedra-robot-t2-group.location
-  service_plan_id = azurerm_service_plan.andre-savedra-robot-t2-sp.id
+resource "azurerm_windows_web_app" "yuri-bot-t-app" {
+  name = "yuri-bot-t-app"
+  resource_group_name = azurerm_resource_group.yuri-bot-t-group.name
+  location = azurerm_resource_group.yuri-bot-t-group.location
+  service_plan_id = azurerm_service_plan.yuri-bot-t-sp.id
   site_config {
     always_on = false
   }
 }
+
+resource "azurerm_windows_web_app_slot" "yuri-bot-t-app-QA" {
+  name = "yuri-bot-t-app-QA"
+  app_service_id = azurerm_windows_web_app.yuri-bot-t-app.id
+  site_config {
+  
+    }
+  }  
