4d0ab7348e8ff224410a40daa71891fe8941580b
YuriHenriqueRezende <yuri.henrique.rezende@outlook.com.br>
2024-11-10 19:07:07 -0300

    commit

--- mange-motos/src/App.vue
+++ mange-motos/src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import Menu from './components/menu.vue';
+import FooterMotos from './components/menu.vue';
 </script>
 
 <template>
@@ -14,7 +15,11 @@ import Menu from './components/menu.vue';
       <RouterView />
     </div>
     <footer class="flex flex-row align-items-end justify-content-end">
-
+      <div class="footer-motos flex flex-row justify-content-evenly">
+        <FooterMotos img="" text=""/>
+        <FooterMotos img="" text=""/>
+        <FooterMotos img="" text=""/>
+      </div>
     </footer>
   </main>
 </template>
@@ -26,11 +31,21 @@ import Menu from './components/menu.vue';
   background-color: var(--app-card-background);
   border-radius: 1.5rem;
 }
+#content-app{
+  height: calc(100% - 10rem);
+}
 footer{
   width: 100%;
   height: 20%;
+  max-height: 10rem;
   background-color: var(--app-footer-background);
   border-radius: 0 0 1.5rem 1.5rem;
+}
+
+@media (max-height: 880px){
+  #content-app{
+    height: 80%;
+  }
 }
 
 </style>
