52d7442347a7f5bfb0801530703fd7fa80d9a3ee
YuriHenriqueRezende <yuri.henrique.rezende@outlook.com.br>
2024-11-10 23:54:27 -0300

    commit

--- mange-motos/src/components/menu.vue
+++ mange-motos/src/components/menu.vue
@@ -4,7 +4,8 @@
 <template>
   <header class="flex flex-row align-items-center justify-content-start">
     <img src="/logo.png" alt="moto">
-    <nav>
+    <nav class="ml-5">
+      <a href="ml-5"></a>
     <RouterLink to="/">Home</RouterLink>
     <RouterLink to="/Conta">Conta</RouterLink>
     <RouterLink to="/Motos">Motos</RouterLink>
@@ -15,6 +16,16 @@
 <style scoped lang="scss">
 header{
   height: 5rem;
+  width: 40rem;
+  img{
+    margin-left: 4rem;
+    height: 4rem;
+  }
+  a{
+    text-decoration: none;
+    font-size: 1.5rem;
+    color: var(--app-dark-text-color);
+  }
 }
 
 </style>
