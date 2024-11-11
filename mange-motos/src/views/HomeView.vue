c959b3c9b358fe5253b3cb87a93e694f8db629f7
YuriHenriqueRezende <yuri.henrique.rezende@outlook.com.br>
2024-11-11 00:44:53 -0300

    commit

--- mange-motos/src/views/HomeView.vue
+++ mange-motos/src/views/HomeView.vue
@@ -11,14 +11,24 @@ export default defineComponent({
   <div class="flex flex-row align-items-center justify-content-evenly">
     <section class="flex flex-column align-items-center">
       <h1>Monte sua moto!</h1>
-      <button>Quero montar</button>
+      <button class="mt-4 app-dark-button" id="order">Quero montar</button>
+    </section>
       <section>
-        <img src="/moto.png" alt="">
-      </section>
+        <img src="/moto.png" alt="moto">
     </section>
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+h1{
+  font-size: 5rem;
+  width: 25rem;
+  text-align: center;
+}
+#order{
+  height: 3.2rem;
+  width: 20rem;
+  border-radius: 2.5rem;
+}
 
 </style>
