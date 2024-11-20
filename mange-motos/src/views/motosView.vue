d7ebe4570201f8971addc87bd35fe4dcf2ebdda1
YuriHenriqueRezende <yuri.henrique.rezende@outlook.com.br>
2024-11-20 14:47:54 -0300

    ainda nao entendi

--- mange-motos/src/views/motosView.vue
+++ mange-motos/src/views/motosView.vue
@@ -1,18 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
 <template>
-    <h1>Imagem de Teste</h1>
-    <!-- Exibindo uma única imagem diretamente -->
-    <img src="http://localhost:3000/cachorro.jpg" alt="Cachorro" />
-  </template>
-  
-  <script setup lang="ts">
-  // Nenhum script necessário para este exemplo simples
-  </script>
-  
-  <style scoped>
-  img {
-    max-width: 300px;
-    border: 2px solid #ccc;
-    border-radius: 8px;
-  }
-  </style>
-  
+<h1>motos</h1>
+</template>
+
+<style scoped lang="scss">
+
+</style>
